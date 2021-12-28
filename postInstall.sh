2e0aa589294378fcfea3304e987cf5ec91591ea4
Lorem96 <sayber1990@gmail.com>
2021-12-28 15:40:01 +0200

    copy default configs

--- postInstall.sh
+++ postInstall.sh
@@ -1,5 +1,20 @@
 cd ~/
 
+echo "Clone repo"
+git clone https://github.com/Lorem96/arch-install.git
+
+echo "Create config dirs"
+mkdir ~/.config/i3
+mkdir ~/.config/polybar
+
+echo "Copy configs"
+cp ~/arch-install/i3/config ~/.config/i3/config
+cp ~/arch-install/polybar/config ~/.config/polybar/config
+
+echo "Download wallpaper"
+mkdir -p ~/.config/wallpaper/
+curl https://clearvision.gitlab.io/images/sapphire.jpg --output "~/.config/wallpaper/wallpaper.jpg"
+
 echo "Setup yay"
 sudo pacman --noconfirm -Syy
 sudo pacman --noconfirm -S --needed git base-devel
