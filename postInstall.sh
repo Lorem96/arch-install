a5b8fd06ce73129e1a46716e7175422f195c898f
Lorem96 <sayber1990@gmail.com>
2021-12-29 14:36:06 +0200

    fix path

--- postInstall.sh
+++ postInstall.sh
@@ -1,22 +1,24 @@
-cd ~/
+cd ~/.
+
+sudo pacman --noconfirm -Sy git
 
 echo "Clone repo"
 git clone https://github.com/Lorem96/arch-install.git
 
 echo "Create config dirs"
-mkdir ~/.config/i3
-mkdir ~/.config/polybar
+mkdir -p ~/.config/i3
+mkdir -p ~/.config/polybar
+mkdir -p ~/.config/wallpaper
 
 echo "Copy configs"
 cp ~/arch-install/i3/config ~/.config/i3/config
 cp ~/arch-install/polybar/config ~/.config/polybar/config
+cp ~/arch-install/polybar/launch.sh ~/.config/polybar/launch.sh
 
 echo "Download wallpaper"
-mkdir -p ~/.config/wallpaper/
-curl https://clearvision.gitlab.io/images/sapphire.jpg --output "~/.config/wallpaper/wallpaper.jpg"
+curl https://clearvision.gitlab.io/images/sapphire.jpg --output ~/.config/wallpaper/wallpaper.jpg
 
 echo "Setup yay"
-sudo pacman --noconfirm -Syy
 sudo pacman --noconfirm -S --needed git base-devel
 git clone https://aur.archlinux.org/yay.git
 cd yay
